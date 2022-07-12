13f11b63cdb8a7900239fef34dc7a7888183e0e7
Egor Shkarin <58490727+Grandschtien@users.noreply.github.com>
2022-07-12 12:51:17 +0300

    Add initial project and assets

---
+++ PartyPoint/PartyPoint/InitialScreen/InitialViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PartyPoint
+//
+//  Created by Егор Шкарин on 12.07.2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
